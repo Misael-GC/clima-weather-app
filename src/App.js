ee4edcad2cacadd85b71b3fda3a6daeb99cf157d
Misael-GC <misael.gomez1623@gmail.com>
2022-08-28 18:09:20 -0500

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { CurrentWeather } from './components/CurrentWheather'
+import { CurrentWeather } from "./components/CurrentWheather";
 
 function App() {
   return (
     <div className="App">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+          <div class="col">Column</div>
+        </div>
+      </div>
       <p>Soy otro componente puedo ser el header</p>
-      <CurrentWeather/>
+      <CurrentWeather />
     </div>
   );
 }
 
 export default App;
-
